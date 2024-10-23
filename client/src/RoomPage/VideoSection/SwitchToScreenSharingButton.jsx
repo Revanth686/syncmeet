5bcbcce83d9ed12966bcc5124bdd947676920da9
Revanth221 <revanth22bcs221@iiitkottayam.ac.in>
2024-10-23 06:36:58 +0530

    completed roomPage buttons

--- client/src/RoomPage/VideoSection/SwitchToScreenSharingButton.jsx
+++ client/src/RoomPage/VideoSection/SwitchToScreenSharingButton.jsx
@@ -1,20 +1,53 @@
 import React, { useState } from "react";
 import SwitchImg from "../../resources/images/switchToScreenSharing.svg";
+import LocalScreenSharingPreview from "./LocalScreenSharingPreview";
+import * as webRTCHandler from "../../utils/webRTCHandler.js";
+
+const constraints = {
+  audio: false,
+  video: true,
+};
 
 const SwitchToScreenSharingButton = () => {
   const [isScreenSharingActive, setIsScreenSharingActive] = useState(false);
+  const [screenSharingStream, setScreenSharingStream] = useState(null);
+
   const handleScreenShareToggle = async () => {
-    console.log(`clicked on screen share button`);
-    setIsScreenSharingActive((prev) => !prev);
+    if (!isScreenSharingActive) {
+      let stream = null;
+      try {
+        stream = await navigator.mediaDevices.getDisplayMedia(constraints);
+      } catch (err) {
+        console.log("error getting screen share stream");
+      }
+      if (stream) {
+        setScreenSharingStream(stream);
+
+        webRTCHandler.toggleScreenShare(isScreenSharingActive, stream);
+        setIsScreenSharingActive(true);
+      }
+    } else {
+      webRTCHandler.toggleScreenShare(isScreenSharingActive);
+      setIsScreenSharingActive(false);
+
+      // stop screen share stream
+      screenSharingStream.getTracks().forEach((t) => t.stop());
+      setScreenSharingStream(null);
+    }
   };
   return (
-    <div className="video_button_container">
-      <img
-        src={SwitchImg}
-        onClick={handleScreenShareToggle}
-        className="video_button_image"
-      />
-    </div>
+    <>
+      <div className="video_button_container">
+        <img
+          src={SwitchImg}
+          onClick={handleScreenShareToggle}
+          className="video_button_image"
+        />
+      </div>
+      {isScreenSharingActive && (
+        <LocalScreenSharingPreview stream={screenSharingStream} />
+      )}
+    </>
   );
 };
 
